9f3e4b7054bf70a6cc2f5ac4953e3329c6174dfe
Andrey K <andreykochnov@yandex.ru>
2017-06-27 21:06:36 +0300

    set background and toolbar

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (QMainWindow, QDesktopWidget,  QWidget, QLCDNumber, QSlider,
     QVBoxLayout, QApplication, QLabel, QHBoxLayout, QComboBox, QAction, QInputDialog, QMessageBox, QPushButton)
-from PyQt5.QtGui import QIcon, QColor
+from PyQt5.QtGui import QIcon, QColor,  QPalette
 import os
 import serial
 from PyQt5.QtCore import Qt
@@ -10,16 +10,28 @@ import time
 class Window (QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        self.setBackground()
+        self.center()
+        
+        self.statusBar =  self.statusBar()
+        self.setWindowTitle('Navigare')
+        self.setWindowIcon(QIcon('icon.jpg'))
+        
+        self.show()
+        
+    def setBackground(self):
+        pal =  QPalette()
+        pal.setColor(QPalette.Background,  Qt.white)
+        self.setAutoFillBackground(True)
+        self.setPalette(pal)        
+    def center(self):
         screen =  QDesktopWidget().screenGeometry()
-        print(screen,  screen.width(),  screen.height())
-        self.resize(screen.width() - 10,  screen.height() - 30)
+        self.resize(screen.width() - 20,  screen.height() - 40)
         size =  self.geometry()
-        print(size)
         x =  (screen.width() - size.width()) / 2
         y = (screen.height() - size.height()) / 2
-        print(x,  y)
-        self.move(x,  y)
-        self.show()
+        self.move(x,  y)        
 if __name__ == "__main__":
     app = QApplication([])
     window = Window()
