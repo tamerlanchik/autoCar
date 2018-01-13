987a5933f60543ff563ef906e1768abbd908089d
Andrey K <andreykochnov@yandex.ru>
2018-01-13 21:46:44 +0300

    /

--- tests/Arduino controller/base/base.ino
+++ tests/Arduino controller/base/base.ino
@@ -3,65 +3,31 @@
 #include <RF24.h>                                         // Подключаем библиотеку  для работы с nRF24L01+
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-class Queue
-{
-private:
-    struct list1
-    {
-        int val;
-        list1* next;
-    };
-    list1* first;
-    list1* last;
-public:
-    Queue()
-    {
-        first=new list1;
-        last = first;
-    }
-    void put(int a)
-    {
-        list1* newLink = new list1;
-        newLink->next = NULL;
-        last->val=a;
-        last->next = newLink;
-        last=newLink;
-    }
-    int get()
-    {
-        if(first->next!=NULL){
-            int res=first->val;
-            list1* temp=first;
-            first=first->next;
-            delete temp;
-            return res;
-        }
-        else{
-            return 0;
-        }
-    }
-    bool isEnd()
-    {
-        if (first->next == NULL)
-            return true;
-        else
-            return false;
-    }
-};
+#include <TimerOne.h>
 class Trans
 {
   private:
-    static const RF24 radio;
-    static const LiquidCrystal_I2C lcd;
+    const RF24 radio;
+    const LiquidCrystal_I2C lcd;
     static const byte LED=8;
     static const byte BUTTON=7;
     static const byte ledComm = 8;
     static const byte repN=20, randMin=1, randMax=10, randK=5;
-    byte data[2];
+    static const int pipeAddress[2]={0x1234567891LL, 0x1234567890LL};
+    static const byte channel = 5;
+    enum {GETINF, GETSEN, CNTRL, SET};
+    static const int serialSpeed = 9600;
+    static const byte msgSize = 5;
+    
+    
+    int data[msgSize];
+    int cntrl[msgSize];
+    int set[msgSize];
+    int getsen[msgSize];
     double del;
     bool butState, butStateTemp, ledState;  
   public:
-    Trans(){}
+    Trans():radio(9, 10), lcd(0x27,16,2){}
     void init(){
       randomSeed(analogRead(0));
       pinMode(BUTTON, INPUT);
@@ -69,20 +35,44 @@ class Trans
 
       butState = digitalRead(BUTTON);
       ledState = false;
+      
       lcd.init();                     
       lcd.backlight();
-      lcd.print("Start");
+      //lcd.print("Start");
+      
+      initRadio(); 
 
+      Serial.begin(serialSpeed);
+    }
+    void initRadio(){
       radio.begin();                                        // Инициируем работу nRF24L01+
-      radio.setChannel(5);                                  // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
+      radio.setChannel(channel);                                  // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
       radio.setRetries(15, 10);
       radio.setDataRate     (RF24_250KBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
       radio.setPALevel      (RF24_PA_HIGH);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
-      radio.openReadingPipe (1, 0x1234567891LL);            // Открываем 1 трубу с идентификатором 0x1234567890 для приема данных (на ожном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
-      radio.openWritingPipe (0x1234567890LL);
+      radio.openReadingPipe (1, pipeAddress[0]);            // Открываем 1 трубу с идентификатором 0x1234567890 для приема данных (на ожном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
+      radio.openWritingPipe (pipeAddress[1]);
       radio.startListening  (); 
+      delay(300);
     }
-    bool writeD(byte dat[], int len){
+    void cpy(int src[], int dest[]){
+      for(int i=0; i<msgSize; i++)
+        dest[i]=src[i];
+    }
+    void readRadio(){
+      bool s, f, r;
+      radio.whatHappened(s, f, r);
+      lcd.print("y");
+      if(r)
+      {
+        while(radio.available())
+        {
+          radio.read(&data, sizeof(data));
+          lcd.print("hppp");
+        }
+      }
+    }
+    bool writeD(int dat[], int len){
       int i=0;
       bool fl=0;
       do{
@@ -96,21 +86,32 @@ class Trans
       lcd.print(i);
       return fl;
     }
+    void readMail()
+    {
+    }
     void checkBut(){
       if(digitalRead(BUTTON)^butState)
       {
         butState=digitalRead(BUTTON);
-        data[0]=ledComm;
+        data[0]=CNTRL;
         data[1]=butState;
         writeD(data, sizeof(data));
       }
     }
     void readBut(){
-      radio.read(&data, sizeof(data));
+      /*radio.read(&data, sizeof(data));
       if(data[0]==ledComm)
       {
         ledState = data[1];
         digitalWrite(LED, ledState);
+      }*/
+      byte temp[1];
+      while(radio.available())
+      {
+        radio.read(&temp, sizeof(temp));
+        lcd.clear();
+        //lcd.print(temp[0]);
+        delay(100);
       }
     }
     bool radioAvailable(){
@@ -120,18 +121,20 @@ class Trans
         return false;
     }
 };
-const RF24 Trans::radio(9, 10);
-const LiquidCrystal_I2C Trans::lcd(0x27,16,2);
 Trans item;
+void readRadio(){Serial.println("t");}
 void setup(){
   item.init();
-  delay(500);
+  //attachInterrupt(1, readRadio, FALLING);
 }
 
 void loop(){
-  item.checkBut();
+  //item.readMail();
+  //item.checkBut();
   if(item.radioAvailable())
   {
     item.readBut();
   }
+  if(digitalRead(3))
+    Serial.println("1");
 }
