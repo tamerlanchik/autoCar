8b9b87b2912e33e6c4011229e450f0ac29ac7c3c
Andrey K <andreykochnov@yandex.ru>
2018-01-17 18:26:34 +0300

    .

--- tests/roboCar - baseController/roboCar - baseController.ino
+++ tests/roboCar - baseController/roboCar - baseController.ino
@@ -1,24 +1,26 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
-#include <libraries/LiquidCrystal_I2C/LiquidCrystal_I2C.h>
+const char motorJoys[2] = {2, 3};
+const char sonarJoy = 2;
+const char signalBut = 3;
+const char scanBut = 4;
+const char movingFlagLED = 5;
+const char scanningFlagLED = 6;
+const char epsM = 10;
+const char epsS = 3;
 class Joysticks{
 private:
-    const byte motorJoys[2] = {2, 3};
-    const int sonarJoy = 1;
-    const byte signalBut = 3;
-    const byte scanBut = 4;
-    const byte epsM = 5;
-    const byte epsS = 3;
-
     int preMotorsVals[2];
-    byte preSonarAngle;
+    char preSonarAngle;
     int motorJoysNeutral[2];
     int sonarJoyNeutral;
+
 
 public:
     bool getMotorsJoys(int val[]) {
         bool isChanged=false;
-        for(byte i=0; i<2; i++){
+        for(char i=0; i<2; i++){
             val[i]=analogRead(motorJoys[i]);
             if(abs(val[i]-preMotorsVals[i])>epsM){
                 isChanged = true;
@@ -29,7 +31,7 @@ public:
     }
     bool getSonarJoy(int val){
         bool isChanged=false;
-        val=analogRead(sonarJoys);
+        val=analogRead(sonarJoy);
         if(abs(val-preSonarAngle)>epsS){
             isChanged = true;
             preSonarAngle = val;
@@ -49,17 +51,25 @@ private:
     //bool isMovingFlag;
     //bool isScanningFlag;
 
-    const LiquidCrystal_I2C lcd;
-    const byte movingFlagLED = 5;
-    const byte scanningFlagLED = 6;
+    LiquidCrystal_I2C lcd;
 public:
-    Indication():lcd(0x27,16,2) {}
-    bool updateLCD(int data[], int len){
+    Indication():lcd(0x27,16,2) {
+      }
+    void initLCD(){
+      uint8_t a[8] = {B00000, B11111, B11111, B11111, B11111, B11111, B11111, B00000};
+      uint8_t b[8] = {B00100, B01010, B01010, B00100, B00000, B00000, B00000, B00000};
+      lcd.init();
+      lcd.backlight();
+      lcd.createChar(1, a);
+      lcd.createChar(2, b);
+    }
+    void updateLCD(int data[], int len){
         lcd.clear();
 
         //Motors values
         lcd.setCursor(0, 0);
         lcd.print("M");
+        lcd.setCursor(2, 0);
         lcd.print(data[0]);
         lcd.setCursor(5, 0);
         lcd.print("|");
@@ -68,23 +78,22 @@ public:
         //Borders values
         lcd.setCursor(11, 0);
         lcd.print("B");
-        for(byte i=0; i<4; i++){
-            lcd.setCursor(11+(i%2)*3, i/2);
+        for(char i=0; i<4; i++){
+            lcd.setCursor(12+(i%2)*3, i/2);
             lcd.print("|");
         }
-        for(byte i=0; i<4; i++){
-            if((data[2]%pow(10, i+1))/pow(10, i)){
-                lcd.setCursor(12+i%2, i/2);
-                lcd.print("\xDB");
+        for(char i=0; i<4; i++){
+            if((data[2]%(int)pow(10, i+1))/(int)pow(10, i)){
+                lcd.setCursor(13+i%2, i/2);
+                lcd.write(1);
             }
         }
 
         //Sonar angle+value
         lcd.setCursor(0, 1);
-        lcd.print("S"); lcd.print(data[3]); lcd.print("°");
+        lcd.print("S"); lcd.setCursor(2, 1);lcd.print(data[3]); lcd.write(2);
         if(data[4]<0) lcd.print("-");
         else          lcd.print(data[4]);
-        return true;
     }
     void setMovingFlagLED(bool flag){
         digitalWrite(movingFlagLED, flag);
@@ -103,23 +112,27 @@ private:
     int sensorData[3];
     int exertData[3];
     Indication indicator;
+    Joysticks control;
 public:
+    Manager(){
+    }
     bool readRadio() {return 0;}
     bool readControl() {return 0;}
     bool setIndication() {
-        int indictionData[] = {123, 321, 1010, 130, 432};
+        int indicationData[] = {1230, 321, 1011, 130, 432};
+        indicator.initLCD();
         indicator.updateLCD(indicationData, 5);
     }
 
     bool sendCommandRadio() {return 0;}
     bool sendCommandSerial() {return 0;}
-    void devSerialEvent() {void}
-    void ascSensors() {void}
+    void devSerialEvent() {char a;}
+    void ascSensors() {char a;}
 };
-Manager manager;
+Manager* manager = new Manager();
 void setup()
 {
-    manager.setIndication();
+    manager->setIndication();
 }
 
 void loop()
