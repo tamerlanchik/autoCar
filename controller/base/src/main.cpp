02082a353685731787491b89c15acb18b583ce33
Ian <andreykochnov@yandex.ru>
2018-01-25 21:37:26 +0300

    Delete TimerInterrupt

--- controller/base/src/main.cpp
+++ controller/base/src/main.cpp
@@ -5,21 +5,15 @@
 extern Logger Log;
 Manager manager;
 int i=1;
-void sendTest(){
-  manager.sendTest();
-}
+
 void setup()
 {
   manager.init();
-  Timer1.initialize(1000000);
-  Timer1.attachInterrupt(sendTest);
 }
 
 void loop()
 {
   if(manager.radioAvailable()){
-    noInterrupts();
     manager.readRadio();
-    interrupts();
   }
 }
